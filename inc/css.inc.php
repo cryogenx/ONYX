ff2ecc6908da3dae9254fe1a2fbda59dcbcd89de
CCC\gwelsted <gwelsted@hernandoclerk.org>
2017-06-22 15:08:03 -0400

    initial coomit

---
+++ inc/css.inc.php
@@ -0,0 +1,6 @@
+<?php ?>
+
+<!-- AngularJS Material CSS now available via Google CDN; version 1.1.4 used here -->
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/angular_material/1.1.4/angular-material.min.css">"
+
+
---
+++ inc/scripts.inc.php
@@ -0,0 +1,9 @@
+<?php ?>
+	<!-- AngularJS Material Dependencies -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.11/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.11/angular-animate.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.11/angular-aria.min.js"></script>
+
+
+    <!-- AngularJS Material Javascript now available via Google CDN; version 1.1.4 used here -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angular_material/1.1.4/angular-material.min.js"></script>
